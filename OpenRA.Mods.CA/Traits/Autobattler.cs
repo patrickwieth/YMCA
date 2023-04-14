428935b4e88201ece5dac9075bcf2f0567a89e77
Patrick <patrick.wieth@googlemail.com>
2023-04-14 20:59:12 +0200

    autobattler first draft

---
+++ OpenRA.Mods.CA/Traits/Autobattler.cs
@@ -0,0 +1,68 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2015- OpenRA.Mods.AS Developers (see AUTHORS)
+ * This file is a part of a third-party plugin for OpenRA, which is
+ * free software. It is made available to you under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation. For more information, see COPYING.
+ */
+#endregion
+
+using System.Linq;
+using OpenRA.Mods.Common.Activities;
+using OpenRA.Mods.CA.Activities;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.CA.Traits
+{
+	[Desc("Can be slaved to a spawner.")]
+	public class AutobattlerInfo : TraitInfo
+	{
+		public override object Create(ActorInitializer init) { return new Autobattler(init, this); }
+	}
+
+	public class Autobattler : ITick, INotifyCreated, INotifyIdle
+	{
+		public readonly AutobattlerInfo Info;
+
+		Actor nextCheckpoint;
+
+		public Autobattler(ActorInitializer init, AutobattlerInfo info)
+		{
+			Info = info;
+		}
+
+		void INotifyCreated.Created(Actor self)
+		{
+			Created(self);
+		}
+
+		protected virtual void Created(Actor self)
+		{
+			nextCheckpoint = self.World.ActorsHavingTrait<Checkpoint>()
+				.Where(a => !a.IsDead).ClosestTo(self);
+		}
+
+		void ITick.Tick(Actor self)
+		{
+
+		}
+
+		public void setNextCheckpoint(Actor cp) {
+
+			nextCheckpoint = cp;
+
+		}
+
+		void INotifyIdle.TickIdle(Actor self)
+		{
+			var move = self.TraitOrDefault<IMove>();
+			if (move != null && nextCheckpoint != null)
+			{
+				var location = self.World.Map.CellContaining(nextCheckpoint.CenterPosition);
+				self.QueueActivity(false, new AttackMoveActivity(self, () => move.MoveTo(location, 0)));
+			}
+		}
+	}
+}
---
+++ OpenRA.Mods.CA/Traits/Checkpoint.cs
@@ -0,0 +1,93 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2015- OpenRA.Mods.AS Developers (see AUTHORS)
+ * This file is a part of a third-party plugin for OpenRA, which is
+ * free software. It is made available to you under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation. For more information, see COPYING.
+ */
+#endregion
+
+using System.Linq;
+using System.Collections.Generic;
+using OpenRA.Mods.Common.Activities;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.CA.Traits
+{
+	[Desc("When enabled, the actor will randomly try to attack nearby other actors.")]
+	public class CheckpointInfo : ProximityCapturableInfo, IEditorActorOptions
+	{
+		[Desc("Display order for the facing slider in the map editor")]
+		public readonly int EditorHierarchyDisplayOrder = 3;
+
+		[Desc("The hierarchy of a checkpoint determines the possible next checkpoints, where a unit can go")]
+		IEnumerable<EditorActorOption> IEditorActorOptions.ActorOptions(ActorInfo ai, World world)
+		{
+			yield return new EditorActorSlider("Hierarchy", EditorHierarchyDisplayOrder, -8, 8, 8,
+				actor =>
+				{
+					var init = actor.GetInitOrDefault<HierarchyInit>();
+					return init?.Value ?? 0;
+				},
+				(actor, value) => actor.ReplaceInit(new HierarchyInit((int)value)));
+		}
+
+		public override object Create(ActorInitializer init) { return new Checkpoint(init, init.Self, this); }
+	}
+
+	class Checkpoint : ProximityCapturable, INotifyIdle
+	{
+		readonly CheckpointInfo info;
+		readonly int Hierarchy;
+
+		public Checkpoint(ActorInitializer init, Actor self, CheckpointInfo info)
+			: base(self, info)
+		{
+			 this.info = info;
+
+			 var hierarchyInit = init.GetOrDefault<HierarchyInit>();
+			 if (hierarchyInit != null)
+				 Hierarchy = hierarchyInit.Value;
+		}
+
+		public override void ActorEntered(Actor other)
+		{
+			TextNotificationsManager.Debug("bla "+other.ToString());
+
+			var autobattler = other.TraitOrDefault<Autobattler>();
+			if (autobattler != null)
+			{
+				var nextCheckpoint = Self.World.ActorsHavingTrait<Checkpoint>()
+					.Where(a => a.TraitOrDefault<Checkpoint>().Hierarchy > Self.TraitOrDefault<Checkpoint>().Hierarchy)
+					.ClosestTo(Self);
+
+				foreach( var cp in Self.World.ActorsHavingTrait<Checkpoint>()
+					) //&& a.TraitOrDefault<Checkpoint>().info.Hierarchy > Self.TraitOrDefault<Checkpoint>().info.Hierarchy) )
+				{
+					TextNotificationsManager.Debug("cp hierarchy:"+cp.TraitOrDefault<Checkpoint>().Hierarchy);
+				}
+
+				TextNotificationsManager.Debug("nextcp:"+nextCheckpoint);
+
+				if (nextCheckpoint != null)
+					autobattler.setNextCheckpoint(nextCheckpoint);
+			}
+
+			base.ActorEntered(other);
+		}
+
+		void INotifyIdle.TickIdle(Actor self)
+		{
+
+		}
+	}
+
+	public class HierarchyInit : ValueActorInit<int>, ISingleInstanceInit
+	{
+		public HierarchyInit(int value)
+			: base(value) { }
+	}
+
+}
