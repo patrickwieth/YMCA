957a736a493a289ac2d6128d944a297cd39c976b
Patrick Wieth <patrick.wieth@googlemail.com>
2024-01-13 23:15:52 +0100

    flickeringpalettes

--- OpenRA.Mods.CA/Traits/FlickeringPaletteEffect.cs
+++ OpenRA.Mods.CA/Traits/FlickeringPaletteEffect.cs
@@ -22,19 +22,20 @@ namespace OpenRA.Mods.CA.Traits
 		[FieldLoader.Require]
 		public readonly string PaletteName;
 
-		[Desc("The basic color used as offset.")]
+		[Desc("Start colour.")]
 		[FieldLoader.Require]
-		public readonly Color BaseColor;
+		public readonly Color StartColor;
 
-		[Desc("Amount of maximum difference on the red channel.")]
-		public readonly int AmplitudeRed = 0;
-		[Desc("Amount of maximum difference on the green channel.")]
-		public readonly int AmplitudeGreen = 0;
-		[Desc("Amount of maximum difference on the blue channel.")]
-		public readonly int AmplitudeBlue = 0;
+		[Desc("End colour.")]
+		[FieldLoader.Require]
+		public readonly Color EndColor;
 
-		public readonly int QuantizationCount = 16;
-		
+		[Desc("Number of ticks to pulse before reversing.")]
+		public readonly int PulseDuration = 25;
+
+		[Desc("Number of ticks to wait on start/end colours.")]
+		public readonly int PulseDelay = 0;
+
 		public readonly int? ShadowIndex = 4;
 
 		public override object Create(ActorInitializer init) { return new FlickeringPaletteEffect(this); }
@@ -43,22 +44,35 @@ namespace OpenRA.Mods.CA.Traits
 	public class FlickeringPaletteEffect : IPaletteModifier, ITick
 	{
 		readonly FlickeringPaletteEffectInfo info;
-		readonly int offset;
 
-		int t;
+		int ticks;
+		bool incrementing;
+
+		int redDiff;
+		int greenDiff;
+		int blueDiff;
+		int alphaDiff;
 
 		public FlickeringPaletteEffect(FlickeringPaletteEffectInfo info)
 		{
 			this.info = info;
-			offset = 1024 / info.QuantizationCount;
+			ticks = 0;
+			incrementing = true;
+
+			redDiff = (info.EndColor.R - info.StartColor.R) / info.PulseDuration;
+			greenDiff = (info.EndColor.G - info.StartColor.G) / info.PulseDuration;
+			blueDiff = (info.EndColor.B - info.StartColor.B) / info.PulseDuration;
+			alphaDiff = (info.EndColor.A - info.StartColor.A) / info.PulseDuration;
 		}
 
 		public void AdjustPalette(IReadOnlyDictionary<string, MutablePalette> b)
 		{
-			// cos value is in range of [-1024, 1024].
-			var red = (info.BaseColor.R + info.AmplitudeRed * WAngle.FromDegrees(t).Cos() / 1024).Clamp(0, 255);
-			var green = (info.BaseColor.G + info.AmplitudeGreen * WAngle.FromDegrees(t).Cos() / 1024).Clamp(0, 255);
-			var blue = (info.BaseColor.B + info.AmplitudeBlue * WAngle.FromDegrees(t).Cos() / 1024).Clamp(0, 255);
+			var pulseTick = (ticks - info.PulseDelay).Clamp(0, info.PulseDuration);
+
+			var red = (info.StartColor.R + (redDiff * pulseTick)).Clamp(0, 255);
+			var green = (info.StartColor.G + (greenDiff * pulseTick)).Clamp(0, 255);
+			var blue = (info.StartColor.B + (blueDiff * pulseTick)).Clamp(0, 255);
+			var alpha = (info.StartColor.A + (alphaDiff * pulseTick)).Clamp(0, 255);
 
 			var p = b[info.PaletteName];
 
@@ -67,14 +81,20 @@ namespace OpenRA.Mods.CA.Traits
 				if (info.ShadowIndex != null && info.ShadowIndex == j)
 					continue;
 
-				var color = Color.FromArgb(info.BaseColor.A, red, green, blue);
+				var color = Color.FromLinear((byte)alpha, red / 255f, green / 255f, blue / 255f);
 				p.SetColor(j, color);
 			}
 		}
 
 		void ITick.Tick(Actor self)
 		{
-			t = (t + offset) % 1024;
+			if (incrementing)
+				ticks++;
+			else
+				ticks--;
+
+			if (ticks == info.PulseDuration + (info.PulseDelay * 2) || ticks == 0)
+				incrementing = !incrementing;
 		}
 	}
 }
